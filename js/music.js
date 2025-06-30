e1148b73d919c2d984f1802435acd59e0b58be77
Thomas <62822072+Thomasluigi07@users.noreply.github.com>
2025-07-01 07:57:12 +1000

    Add modern browser bg music support

--- js/music.js
+++ js/music.js
@@ -1,13 +1,76 @@
-var DontEvenBother = true;
+var CanPlayMusic = true;
 
-function check() {
+function soundTest() {
     try {
-        var ThisIsNotUsedLmao = window.crossOriginIsolated;
-        DontEvenBother = false;
+        var audioElement = document.createElement('audio');
+        return !!(audioElement.canPlayType && audioElement.canPlayType('audio/mpeg;').replace(/no/, ''));
     }
     catch {
-        // Use alerts. Remove iframe.
-        DontEvenBother = true;
-        return
+        return false;
     }
+}
+
+function initAudio() {
+    CanPlayMusic = soundTest();
+    if (CanPlayMusic) {
+        var music = document.getElementById('music');
+        if (music) {
+            var musicPlayer = document.createElement('audio');
+            musicPlayer.id = "musicPlayer";
+            musicPlayer.setAttribute("autoplay", "autoplay");
+            musicPlayer.setAttribute("loop", "loop");
+            musicPlayer.setAttribute("muted", "muted");
+            musicPlayer.setAttribute("src", music.content);
+            var musicControl = document.createElement('img');
+            musicControl.id = "musicControl";
+            musicControl.className = "paused";
+            try {
+                musicControl.onclick = function() {toggleMusic(musicPlayer,musicControl);};
+            } catch {
+                if(musicControl.addEventListener){
+                    musicControl.addEventListener('click', function(){
+                        toggleMusic(musicPlayer,musicControl);
+                    });
+                } else if(musicControl.attachEvent){
+                    musicControl.attachEvent('onclick', function(){
+                        toggleMusic(musicPlayer,musicControl);
+                    });
+                } else {
+                    console.log("Couldn't set up onclick")
+                }
+            }
+            var a = document.getElementsByClassName('header');
+            var header = a[0];
+            document.querySelector('container').appendChild(musicPlayer);
+            document.querySelector('footer').children[0].appendChild(musicControl);
+        }
+    }
+}
+
+function toggleMusic(musicPlayer,musicControl) {
+    if (musicPlayer) {
+        if (musicPlayer.paused) {
+            musicPlayer.play();
+            musicPlayer.muted = false;
+            if (musicControl) {
+                musicControl.className = "playing";
+            }
+        } else {
+            musicPlayer.pause();
+            musicPlayer.muted = true;
+            if (musicControl) {
+                musicControl.className = "paused";
+            }
+        }
+    }
+}
+
+if(window.addEventListener){
+    window.addEventListener('load', function(){
+        initAudio();
+    });
+} else if(window.attachEvent){
+    window.attachEvent('onload', function(){
+        initAudio();
+    });
 }
